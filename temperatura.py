c40e395ea2336ad2d5d246832044011c644b4ffb
dannyardila <dannyardila059@gmail.com>
2022-04-21 18:41:52 -0500

    Desarrollo: creacion diagrama de flujo

--- temperatura.py
+++ temperatura.py
@@ -6,11 +6,11 @@ print("---grados Celsius a Fareheint y Kelvin")
 print("----")
 
 #input
-C=int(input("digite el valor de c")) 
+C=int(input("digite el valor de c ")) 
 
 #processing
-k=c+273.15
-f=c+1.8+32
+k=C+273.15
+f=C+1.8+32
 
 #output
-print(str(c)+ " grados celcius son  " + str(k)+ " grados kelvin y " + str(f) + " grados frahient ")
+print(str(C)+ " grados celcius son  " + str(k)+ " grados kelvin y " + str(f) + " grados frahient ")
